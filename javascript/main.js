0ddfb79205013af2ef797e4dfc7dee1976b21e1c
haim arka <haimarka1@gmail.com>
2021-08-05 15:44:21 +0300

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,216 @@
+//1
+// var num1;
+// var num2;
+// num1 = Number(prompt("enter your number"));
+// num2 = Number(prompt("enter your number"));
+// console.log(num1,num2);
+  
+// if(num1+num2 >= 13){
+// console.log(" bar mitzva");
+//     }
+//     else if(num1+num2 == 12){
+//     console.log(" bat mitzva");
+//         }
+//         else{
+//         console.log( "kid");
+//         }
+
+    //2
+    // var i = 0;
+    // while(i < 100){
+    //     console.log("yes");
+    //     i++;
+    // }
+
+    //3
+    // var i = 0
+    // var num;
+    // num = Number(prompt("enter your number"));
+    // while(i < num){
+    //     console.log("no");
+    //     i++;
+    // }
+
+    //5
+    // for(var i = 0;i < 1000;i++){
+    //     console.log("javascript is not JAVA");
+    // }
+
+    //6
+    // var name1;
+    // var num;
+    // name1 = prompt("enter your name");
+    // num = Number(prompt("enter your number"));
+    // for(var i = 0; i < num;i++){
+    //     console.log(name1);
+    // }
+
+    //7
+    // var email;
+    // var age;
+    // var num;
+    // email = prompt("enter your email");
+    // age = Number(prompt("enter your age"));
+    // num = Number(prompt("enter your number"));
+    // console.log(email,age);
+    // for(var i = 0;i < num;i++){
+    //     if(age >= 18){
+    //         console.log(email,age);
+    //     }
+    //     else{
+    //         console.log("sorry");
+    //     }
+    // }
+
+    //8
+    // var num;
+    // var name1;
+    // var myName = "haim";
+    // num = Number(prompt("enter your number"));
+    // for(var i = 0;i < num;i++){
+    //     name1 = prompt("enter your name");
+    //     console.log(num);
+    //     if(name1 == myName){
+    //         console.log("hi boss " + name1);
+    //     }
+    //     else{
+    //         console.log("no entry");
+    //     }
+    // }
+
+    //9
+    // var num1;
+    // var num2;
+    // num1 = Number(prompt("enter first number"));
+    // num2 = Number(prompt("enter second number"));
+    // for(var i = 0;i < num1;i++){
+    //     for(var j =0;j < num2;j++)
+    //     console.log(i*j);
+    // }
+
+    //10
+    // var array = [1,2,3,4,5]
+    // console.log(array);
+
+    //11
+    // var array = ["haim", "ezra","nadav","uri","oshrat" ]
+    // console.log(array);
+
+    //12
+    // var array = [20,55,17,90,32,12,71,84]
+    // for(var i = 0;i < array.length;i++){
+    //     if(array[i] % 2 == 0){
+    //         console.log(array[i]);
+    //     }
+    //     else {
+    //         console.log("not zugi");
+    //     }
+    // }
+
+
+    //13
+    // var array = []
+    // for(var i = 0;i < 5;i++){
+    //     var name1 = prompt("enter your name");
+    //     // console.log(name1);
+    //     console.log(array);
+    //     array.push(name1)
+
+    // }
+
+    //14
+
+    // var num;
+    // var array = []
+    // for(var i = 0;i < 7;i++){
+    //     num = Number(prompt("enter your number"));
+    //     if (num % 2 != 0){
+    //         console.log(array[i]);
+    //         array.push(num)
+    //     }
+    //     else{
+    //         console.log("not work");
+    //     }
+    //     console.log(array);
+    // }
+
+    //15
+
+    // var array = [1,2," haim",3," nadav",4];
+    // var num;
+    // for(var i = 0;i < array.length;i++){
+    //     if(Number(array[i])){
+    //         console.log(array[i]);
+    //     }
+    //     else {
+    //         console.log("not number");
+    //     }
+    // }
+
+    //16
+
+    // var i = 0;
+    // var num;
+    // var array = [];
+    // while(i < 10){
+    //     num = Number(prompt("enter number"));
+    //     if(num % 2 == 0){
+    //         console.log(array);
+    //         array.push(num)
+    //         i++;
+    //     }
+    //     else{
+    //         console.log("E zugi");
+    //     }
+    // }
+
+    //17
+
+//    var heightRectangle;
+//    var widthRectangle;
+//    var num;
+//    var area;
+//    var scope;
+//    heightRectangle = Number(prompt("enter height number"));
+//    widthRectangle = Number(prompt("enter width number"));
+//    num = Number(prompt("enter last number"));
+//    area = heightRectangle * widthRectangle;
+//    console.log(area);
+//    scope =  heightRectangle + widthRectangle;
+//    console.log(scope);
+//    console.log(num);
+
+   //18
+
+//    num1 = Number(prompt("enter first number"));
+//    num2 = Number(prompt("enter middle number"));
+//    num3 = Number(prompt("enter last number"));
+
+//    if(num2 > num1 && num3 > num2){
+//        console.log("going up");
+   
+//     }
+//        else if(num2 < num1 && num3 < num2){
+//            console.log("going down");
+//        }
+//             else{
+//                 console.log("no order");
+//             }
+
+
+    //19 and last
+
+//     var num;
+//     var modul;
+//     num = Number(prompt("enter your number"));
+//     console.log(num);
+//     while(num > 0){
+//     modul = num % 10;
+//     num = parseInt(num / 10);
+//     console.log(modul);
+// }
+
+
+
+
+
